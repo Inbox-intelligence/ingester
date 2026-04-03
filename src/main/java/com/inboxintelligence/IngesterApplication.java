c7c269b949b05f8655a521d217fb3964fa8e9b3c
Ankit Raj <rja.ankit@gmail.com>
2026-04-04 01:25:55 +0530

    persistence -lib changes

---
+++ src/main/java/com/inboxintelligence/IngesterApplication.java
@@ -0,0 +1,14 @@
+package com.inboxintelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class IngesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngesterApplication.class, args);
+    }
+}
