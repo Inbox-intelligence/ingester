bbe28c2a00405a99d1810b019afa0c81827e8395
Ankit Raj <rja.ankit@gmail.com>
2026-04-16 13:17:22 +0530

    refactor

--- src/test/java/com/inboxintelligence/ingester/IngesterApplicationTests.java
+++ src/test/java/com/inboxintelligence/ingester/IngesterApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class IngesterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
